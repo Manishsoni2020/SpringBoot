a6fb2ccfe3776803c9f5b8fb329b3ecf5e4440ca
Manishsoni2020 <sonimanish24163@gmail.com>
2025-03-22 15:44:19 +0530

    SPRING BASIC

---
+++ FirstSpringjavaproject/src/in/ms/main/Main.java
@@ -0,0 +1,26 @@
+package in.ms.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.ms.beans.Student;
+
+public class Main {
+	public static void main(String[] args) {
+		String config_loc="/in/ms/resourse/applicationContext.xml";
+		ApplicationContext context = new ClassPathXmlApplicationContext(config_loc);
+		Student std=(Student)context.getBean("stdId");
+		std.display();
+		
+		System.out.println("==========================");
+		
+		Student std1=(Student)context.getBean("stdId2");
+		std1.display();
+	}
+}
+
+// spring-bean-xxx.jar
+// cz-xxx.jar
+// spring-context-xxx.jar
+// common-logging-xxx.jar
+// spring-expression-xxx.jar
